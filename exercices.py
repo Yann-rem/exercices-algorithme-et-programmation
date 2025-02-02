3047e3c7c90d8b6500c7abba3df3bf2698137527
Yannick Remy <remyyannick@sfr.fr>
2025-02-02 17:39:11 +0100

    Exercice 1

---
+++ exercices.py
@@ -0,0 +1,22 @@
+# %%
+
+# Exercice 1
+a = 10
+b = 10
+
+# Calcul des résultats
+addition = a + b
+soustraction = a - b
+
+if b != 0:
+    division = a / b
+else:
+    division = "Indéfini (division par zéro)"
+
+multiplication = a * b
+
+# Affichage des résultats
+print(f"Addition : {a} + {b} = {addition}")
+print(f"Soustraction : {a} - {b} = {soustraction}")
+print(f"Division : {a} / {b} = {division}")
+print(f"Multiplication : {a} * {b} = {multiplication}")
